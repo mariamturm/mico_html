676d16b8712e2e779bf924a1ca4796a67ebddbe5
mari <chxlartaiko@gmail.com>
2024-10-16 18:23:35 +0400

    improvements to appointments.php

--- appointment.php
+++ appointment.php
@@ -8,14 +8,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $inputSymptoms = htmlspecialchars($_POST['inputSymptoms']);
     $inputDate = htmlspecialchars($_POST['inputDate']);
 
+
+    // Place to put appointments
+    $file = "appointments.txt";
+
+    // What each line of the appointment should say:
+    $dataPatient = "Patient: $inputPatientName" . PHP_EOL;
+    $dataDoctor = "Doctor: $inputDoctorName" . PHP_EOL;
+    $dataDepartment = "Department: $inputDepartmentName" . PHP_EOL;
+    $dataPhone = "Phone: $inputPhone" . PHP_EOL;
+    $dataSymptoms = "Symptoms: $inputSymptoms" . PHP_EOL;
+    $dataDate = "Date: $inputDate\n" . PHP_EOL;
+
+    // add each line to appointment.txt 
+    file_put_contents($file, $dataPatient, FILE_APPEND);
+    file_put_contents($file, $dataDoctor, FILE_APPEND);
+    file_put_contents($file, $dataDepartment, FILE_APPEND);
+    file_put_contents($file, $dataPhone, FILE_APPEND);
+    file_put_contents($file, $dataSymptoms, FILE_APPEND);
+    file_put_contents($file, $dataDate, FILE_APPEND);
+
+
     // Return HTML for the popup
     echo "
-
-
-
-
-   
-
     <div class=\"book_section\">
         <div class=\"appointment-size\">
             <h4> <span> APPOINTMENT </span> BOOKED!</h4>
